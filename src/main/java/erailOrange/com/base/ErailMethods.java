7400ff4bc79dbd1eabed0f428426325df7152186
rohit88kmr19 <rohit88.kmr19@gmail.com>
2024-12-24 15:30:53 +0530

    POM updated with allure pluginsv1.0

--- src/main/java/erailOrange/com/base/ErailMethods.java
+++ src/main/java/erailOrange/com/base/ErailMethods.java
@@ -1,15 +1,7 @@
 package erailOrange.com.base;
 
 import erailOrange.com.utils.PropertyReader;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-import java.util.List;
-
-import static erailOrange.com.driver.DriverManager.driver;
 import static erailOrange.com.driver.DriverManager.getDriver;
 
 
